e3809f75ad649f28819fab81702b441e45c2e777
Hadi Norouzi <hadinorouzi78@gmail.com>
2024-08-15 01:09:41 +0200

    #110 Add ShowBookmarks in ShowRepository

--- data/tmdb-shows/src/main/java/io/filmtime/data/tmdb/shows/TmdbShowsRepositoryImpl.kt
+++ data/tmdb-shows/src/main/java/io/filmtime/data/tmdb/shows/TmdbShowsRepositoryImpl.kt
@@ -4,17 +4,23 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import io.filmtime.data.api.tmdb.TmdbShowsRemoteSource
+import io.filmtime.data.database.dao.BookmarksDao
 import io.filmtime.data.model.EpisodeThumbnail
 import io.filmtime.data.model.GeneralError
 import io.filmtime.data.model.Person
 import io.filmtime.data.model.Result
+import io.filmtime.data.model.Result.Failure
+import io.filmtime.data.model.Result.Success
 import io.filmtime.data.model.VideoDetail
 import io.filmtime.data.model.VideoThumbnail
+import io.filmtime.data.model.VideoType.Show
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 internal class TmdbShowsRepositoryImpl @Inject constructor(
   private val tmdbShowsRemoteSource: TmdbShowsRemoteSource,
+  private val bookmarksDao: BookmarksDao,
 ) : TmdbShowsRepository {
 
   override suspend fun showDetails(showId: Int): Result<VideoDetail, GeneralError> =
@@ -73,4 +79,17 @@ internal class TmdbShowsRepositoryImpl @Inject constructor(
 
   override suspend fun episodesBySeason(showId: Int, seasonNumber: Int): Result<List<EpisodeThumbnail>, GeneralError> =
     tmdbShowsRemoteSource.episodesBySeason(showId, seasonNumber)
+
+  override suspend fun getBookmarkedShows(): Flow<List<VideoThumbnail>> {
+    return bookmarksDao.getAllBookmarksByType(Show).map { bookmarks ->
+      val showsList = mutableListOf<VideoThumbnail>()
+      for (bookmark in bookmarks) {
+        when (val detail = showDetails(bookmark.tmdbId)) {
+          is Failure -> continue
+          is Success -> showsList.add(detail.data.toVideoThumbnail())
+        }
+      }
+      showsList
+    }
+  }
 }
---
+++ data/tmdb-shows/src/main/java/io/filmtime/data/tmdb/shows/VideoDetailExt.kt
@@ -0,0 +1,15 @@
+package io.filmtime.data.tmdb.shows
+
+import io.filmtime.data.model.VideoDetail
+import io.filmtime.data.model.VideoId
+import io.filmtime.data.model.VideoThumbnail
+import io.filmtime.data.model.VideoType.Movie
+
+fun VideoDetail.toVideoThumbnail(): VideoThumbnail =
+  VideoThumbnail(
+    title = title,
+    type = Movie,
+    year = year,
+    ids = VideoId(tmdbId = ids.tmdbId, traktId = ids.traktId),
+    posterUrl = posterUrl,
+  )
