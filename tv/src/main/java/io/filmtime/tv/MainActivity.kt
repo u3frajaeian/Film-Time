c67c2eb90d29ac06d12b6ceccd45a3b8ffaf93ac
Ramin Karimi <raminkarimi1378@gmail.com>
2024-08-30 22:47:58 +0330

    Add tv module

---
+++ tv/src/main/java/io/filmtime/tv/MainActivity.kt
@@ -0,0 +1,48 @@
+package io.filmtime.tv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.Surface
+import androidx.tv.material3.Text
+import io.filmtime.tv.ui.theme.FilmTimeTheme
+
+class MainActivity : ComponentActivity() {
+  @OptIn(ExperimentalTvMaterial3Api::class)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      FilmTimeTheme {
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          shape = RectangleShape,
+        ) {
+          Greeting("Android")
+        }
+      }
+    }
+  }
+}
+
+@OptIn(ExperimentalTvMaterial3Api::class)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  Text(
+    text = "Hello $name!",
+    modifier = modifier,
+  )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  FilmTimeTheme {
+    Greeting("Android")
+  }
+}
