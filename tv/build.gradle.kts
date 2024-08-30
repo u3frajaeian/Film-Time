eb82904d341d65257485b17354b0e1f01fa96ef4
Ramin Karimi <raminkarimi1378@gmail.com>
2024-08-31 00:11:00 +0330

    Add gradle convention in tv module

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -1,40 +1,14 @@
 plugins {
-  alias(libs.plugins.com.android.application)
-  alias(libs.plugins.org.jetbrains.kotlin.android)
-  alias(libs.plugins.compose)
+  id("io.filmtime.gradle.android.application")
 }
 
 android {
   namespace = "io.filmtime.tv"
-  compileSdk = 34
 
   defaultConfig {
     applicationId = "io.filmtime.tv"
-    minSdk = 27
-    targetSdk = 34
-    versionCode = 1
-    versionName = "1.0"
-    vectorDrawables {
-      useSupportLibrary = true
-    }
   }
 
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-    }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-  }
-  kotlinOptions {
-    jvmTarget = "1.8"
-  }
-  buildFeatures {
-    compose = true
-  }
   packaging {
     resources {
       excludes += "/META-INF/{AL2.0,LGPL2.1}"
