fdeb1d64bde1b7ce1788ecce7d883837594357ee
Ramin Karimi <raminkarimi1378@gmail.com>
2024-09-10 00:32:57 +0330

    Remove unused codes

--- tv/src/main/java/io/filmtime/tv/ui/theme/Theme.kt
+++ tv/src/main/java/io/filmtime/tv/ui/theme/Theme.kt
@@ -2,12 +2,10 @@ package io.filmtime.tv.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
-import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.darkColorScheme
 import androidx.tv.material3.lightColorScheme
 
-@OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
 fun FilmTimeTheme(
   isInDarkTheme: Boolean = isSystemInDarkTheme(),
