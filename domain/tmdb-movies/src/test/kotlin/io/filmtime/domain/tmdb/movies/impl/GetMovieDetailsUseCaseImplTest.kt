624a9e7e985986c82dc53d371d571e14577c02c6
m.rajaeian <m.rajaeian@ernyka.com>
2024-10-22 18:39:40 +0330

    Write Unit test for GetMovieDetailUseCase

--- domain/tmdb-movies/src/test/kotlin/io/filmtime/domain/tmdb/movies/impl/GetMovieDetailsUseCaseImplTest.kt
+++ domain/tmdb-movies/src/test/kotlin/io/filmtime/domain/tmdb/movies/impl/GetMovieDetailsUseCaseImplTest.kt
@@ -5,15 +5,12 @@ import io.filmtime.data.model.GeneralError.NetworkError
 import io.filmtime.data.model.GeneralError.UnknownError
 import io.filmtime.data.model.Result.Failure
 import io.filmtime.data.model.Result.Success
-import io.filmtime.data.model.VideoDetail
 import io.filmtime.data.model.VideoId
 import io.filmtime.data.model.VideoThumbnail
 import io.filmtime.data.model.VideoType
 import io.filmtime.domain.tmdb.movies.GetMovieDetailsUseCase
 import io.fimltime.data.tmdb.movies.TmdbMovieRepository
 import junit.framework.TestCase.assertTrue
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
@@ -26,17 +23,17 @@ import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import kotlin.test.assertEquals
 
-class GetMovieDetailsUseCaseImplTest{
-  private lateinit var getMoviesDetailUseCase:GetMovieDetailsUseCase
+class GetMovieDetailsUseCaseImplTest {
+  private lateinit var getMoviesDetailUseCase: GetMovieDetailsUseCase
+
   @Mock
   private lateinit var repository: TmdbMovieRepository
 
   @Before
   fun setUp() {
-  MockitoAnnotations.openMocks(this)
-  getMoviesDetailUseCase=GetMovieDetailsUseCaseImpl(repository)
+    MockitoAnnotations.openMocks(this)
+    getMoviesDetailUseCase = GetMovieDetailsUseCaseImpl(repository)
   }
-
 
   @Test
   fun `should return error when repository returns failure`() = runBlocking {
@@ -49,30 +46,13 @@ class GetMovieDetailsUseCaseImplTest{
 
     errorStates.forEachIndexed { i, error ->
       val expectedResult = Failure(error)
-      `when`(repository.getMovieDetails(movieId)).thenReturn(flowOf( expectedResult))
-      `when`(repository.fetchMovieDetailsFromNetwork(movieId)).thenReturn(flowOf( expectedResult))
-      val result =  getMoviesDetailUseCase.invoke(movieId).toList()
+      `when`(repository.getMovieDetails(movieId)).thenReturn(flowOf(expectedResult))
+//      `when`(repository.fetchMovieDetailsFromNetwork(movieId)).thenReturn(flowOf(expectedResult))
+      val result = getMoviesDetailUseCase.invoke(movieId).toList()
 
       assertTrue(result[0] is Failure)
       verify(repository, times(i + 1)).getMovieDetails(movieId)
       assertEquals(error, (result[0] as Failure).error)
     }
-  }
-
-  @Test
-  fun `call getMovie and get similar as well`() = runBlocking {
-    val movieId = 1
-    val similarMovies = listOf(
-      VideoThumbnail(VideoId(1, 0), "url1", "posterUrl", 2000, VideoType.Movie),
-      VideoThumbnail(VideoId(2, 0), "url2", "posterUrl2", 2000, VideoType.Movie),
-    )
-    val expectedResult = Success(similarMovies)
-    `when`(repository.getMovieDetails(movieId)).thenReturn(expectedResult)
-
-    val result = getMoviesDetailUseCase.invoke(movieId)
-
-    assertTrue(result is Success)
-    verify(repository).getSimilar(movieId)
-    assertEquals(expectedResult, result)
   }
 }
