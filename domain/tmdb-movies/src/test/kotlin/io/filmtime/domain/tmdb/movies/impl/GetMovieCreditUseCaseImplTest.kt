7631bffd140d153caab166a984467ad3038da715
m.rajaeian <m.rajaeian@ernyka.com>
2024-10-22 14:12:50 +0330

    Write Unit test for GetMovieCreditUseCase

--- domain/tmdb-movies/src/test/kotlin/io/filmtime/domain/tmdb/movies/impl/GetMovieCreditUseCaseImplTest.kt
+++ domain/tmdb-movies/src/test/kotlin/io/filmtime/domain/tmdb/movies/impl/GetMovieCreditUseCaseImplTest.kt
@@ -1,5 +1,6 @@
 package io.filmtime.domain.tmdb.movies.impl
 
+import io.filmtime.data.model.CreditItem
 import io.filmtime.data.model.GeneralError.ApiError
 import io.filmtime.data.model.GeneralError.NetworkError
 import io.filmtime.data.model.GeneralError.UnknownError
@@ -8,7 +9,7 @@ import io.filmtime.data.model.Result.Success
 import io.filmtime.data.model.VideoId
 import io.filmtime.data.model.VideoThumbnail
 import io.filmtime.data.model.VideoType
-import io.filmtime.domain.tmdb.movies.GetSimilarUseCase
+import io.filmtime.domain.tmdb.movies.GetMovieCreditUseCase
 import io.fimltime.data.tmdb.movies.TmdbMovieRepository
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.runBlocking
@@ -21,8 +22,9 @@ import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import kotlin.test.assertEquals
 
-class GetSimilarUseCaseImplTest {
-  private lateinit var getSimilarUseCase: GetSimilarUseCase
+class GetMovieCreditUseCaseImplTest {
+
+  private lateinit var getMovieCreditUseCase: GetMovieCreditUseCase
 
   @Mock
   private lateinit var repository: TmdbMovieRepository
@@ -30,7 +32,7 @@ class GetSimilarUseCaseImplTest {
   @Before
   fun setUp() {
     MockitoAnnotations.openMocks(this)
-    getSimilarUseCase = GetSimilarUseCaseImpl(repository)
+    getMovieCreditUseCase = GetMovieCreditUseCaseImpl(repository)
   }
 
   @Test
@@ -43,29 +45,29 @@ class GetSimilarUseCaseImplTest {
     )
     errorStates.forEachIndexed { i, error ->
       val expectedResult = Failure(error)
-      `when`(repository.getSimilar(movieId)).thenReturn(expectedResult)
-      val result = getSimilarUseCase.invoke(movieId)
+      `when`(repository.getCredit(movieId)).thenReturn(expectedResult)
+      val result = getMovieCreditUseCase.invoke(movieId)
 
       assertTrue(result is Failure)
-      verify(repository, times(i + 1)).getSimilar(movieId)
+      verify(repository, times(i + 1)).getCredit(movieId)
       assertEquals(error, (result as Failure).error)
     }
   }
 
   @Test
-  fun `call getSimilar and get similar as well`() = runBlocking {
+  fun `call getMovieCredit and get credits as well`() = runBlocking {
     val movieId = 1
-    val similarMovies = listOf(
-      VideoThumbnail(VideoId(1, 0), "url1", "posterUrl", 2000, VideoType.Movie),
-      VideoThumbnail(VideoId(2, 0), "url2", "posterUrl2", 2000, VideoType.Movie),
-    )
-    val expectedResult = Success(similarMovies)
-    `when`(repository.getSimilar(movieId)).thenReturn(expectedResult)
+    val movieCredit = listOf(
+      CreditItem(1,"ActorName","ProfilePoster")
 
-    val result = getSimilarUseCase.invoke(movieId)
+    )
+    val expectedResult = Success(movieCredit)
+    `when`(repository.getCredit(movieId)).thenReturn(expectedResult)
+
+    val result = getMovieCreditUseCase.invoke(movieId)
 
     assertTrue(result is Success)
-    verify(repository).getSimilar(movieId)
+    verify(repository).getCredit(movieId)
     assertEquals(expectedResult, result)
   }
 }
