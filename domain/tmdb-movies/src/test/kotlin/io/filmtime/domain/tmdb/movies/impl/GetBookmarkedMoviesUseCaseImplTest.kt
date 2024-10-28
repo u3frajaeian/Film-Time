04df606ced7a17954dfd15f62e166ba8ec74c4d6
m.rajaeian <m.rajaeian@ernyka.com>
2024-10-28 11:56:15 +0330

    -Write Unit test for Write Unit test for GetBookmarkedMoviesUseCase

---
+++ domain/tmdb-movies/src/test/kotlin/io/filmtime/domain/tmdb/movies/impl/GetBookmarkedMoviesUseCaseImplTest.kt
@@ -0,0 +1,50 @@
+package io.filmtime.domain.tmdb.movies.impl
+
+import io.filmtime.data.model.GeneralError.ApiError
+import io.filmtime.data.model.GeneralError.NetworkError
+import io.filmtime.data.model.GeneralError.UnknownError
+import io.filmtime.data.model.Result.Failure
+import io.filmtime.data.model.VideoId
+import io.filmtime.data.model.VideoThumbnail
+import io.filmtime.data.model.VideoType
+import io.filmtime.data.model.VideoType.Movie
+import io.filmtime.domain.tmdb.movies.GetBookmarkedMoviesUseCase
+import io.fimltime.data.tmdb.movies.TmdbMovieRepository
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import kotlin.test.assertEquals
+
+class GetBookmarkedMoviesUseCaseImplTest {
+  private lateinit var getBookmarkedMoviesUseCase: GetBookmarkedMoviesUseCase
+
+  @Mock
+  private lateinit var repository: TmdbMovieRepository
+
+  @Before
+  fun setUp() {
+    MockitoAnnotations.openMocks(this)
+    getBookmarkedMoviesUseCase = GetBookmarkedMoviesUseCaseImpl(repository)
+  }
+
+  @Test
+  fun `should return bookmarked movies from local db`(): Unit = runBlocking {
+    val bookmarkedMovies = listOf(
+      VideoThumbnail(VideoId(1, 0), "url1", "posterUrl", 2000, Movie),
+      VideoThumbnail(VideoId(2, 0), "url2", "posterUrl2", 2000, Movie),
+    )
+    `when`(repository.getBookmarkedMovies()).thenReturn(flowOf(bookmarkedMovies))
+    var result = getBookmarkedMoviesUseCase.invoke().toList()
+    assertEquals(bookmarkedMovies,result[0])
+    verify(repository).getBookmarkedMovies()
+  }
+
+}
